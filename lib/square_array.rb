efaf67d99915b93e32e5b8252baaad54e65c3f9f
Rianna Cleary <rianna.cleary@gmail.com>
2019-11-03 22:47:55 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,7 +2,8 @@ def square_array(array)
   counter = 0 
   
   while array[counter] do
-    array[counter] ** 2
+    new_array = []
+    new_array << array[counter] ** 2
     counter += 1 
   end 
 end
